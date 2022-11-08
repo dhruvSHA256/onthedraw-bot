ed133b27e314a671c2f163f11df962e198cee795
DHruv <dhruv.sha256@gmail.com>
2022-11-08 14:36:24 +0530

    feat(main.py): run send and fetch functions asynchronously

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
+import asyncio
 import os
 import random
 import shutil
-from time import sleep
 
 from whatsapp_bot.bot import WABot
 
 IMAGE_DIR = "/home/dhruv/r/scrapy-tutorial/images"
-DELAY = 60
+DELAY = 10
 
 
-def get_new_cartoon():
-    return random.choice(os.listdir(IMAGE_DIR))
+async def fetch():
+    print("fetching data")
+    await asyncio.sleep(DELAY)
 
 
-def main():
-    bot = WABot()
-    bot.start()
-    name = "_"
-    bot.select_contact(name)
-    while True:
+async def send(bot, name):
+    def get_new_cartoon():
+        return random.choice(os.listdir(IMAGE_DIR))
+
+    try:
+        print("sending msg")
+        bot.select_contact(name)
         file_to_send = get_new_cartoon()
         file_location = os.path.join(IMAGE_DIR, file_to_send)
         bot.send_attachment(file_location)
@@ -26,7 +28,25 @@ def main():
             os.path.join(IMAGE_DIR, file_to_send),
             os.path.join(f"{IMAGE_DIR}/used", file_to_send),
         )
-        sleep(DELAY)
+    except Exception:
+        print("An exception occurred")
+    finally:
+        await asyncio.sleep(DELAY)
 
 
-main()
+async def main():
+
+    bot = WABot()
+    bot.start()
+    name = "_"
+    while True:
+        f1 = asyncio.create_task(fetch())
+        f2 = asyncio.create_task(send(bot, name))
+        await asyncio.wait([f1, f2])
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
